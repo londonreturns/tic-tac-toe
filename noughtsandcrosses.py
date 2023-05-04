06973164883f8227e810d9bfc4480f4c71ca9869
londonreturns <subedioscar.47@gmail.com>
2023-05-04 22:12:14 +0545

    file split

--- noughtsandcrosses.py
+++ noughtsandcrosses.py
@@ -1,24 +1,9 @@
-"""
-This program is Tic Tac Toe.
-This is a single plyer game, and the opponent is Computer.
-To win, player or computer must have their marks in,
-any row or any column or any diagonal.
-"""
+"""This is noughtsandcrosses.py module"""
 
-import random # import random for computer's choice
-import json # loads and dumps leaderboard data
-
-
-def initialise_board(board):
-    """
-    This is initialize_board function.
-    Sets all elements to ' '.
-    Returns board.
-    """
-    board = [   [' ', ' ', ' '],
-                [' ', ' ', ' '],
-                [' ', ' ', ' ']] # sets all marks to space string ' '
-    return board
+import random
+from os.path import exists
+import json
+random.seed()
 
 def draw_board(board):
     """
@@ -42,6 +27,17 @@ Welcome to the "Unbeatable Noughts and Crosses" game
 The board layout is shown below:''')
     draw_board(board)
     print('When prompted, enter the number of corresponding to the square you want.')
+
+def initialise_board(board):
+    """
+    This is initialize_board function.
+    Sets all elements to ' '.
+    Returns board.
+    """
+    board = [   [' ', ' ', ' '],
+                [' ', ' ', ' '],
+                [' ', ' ', ' ']] # sets all marks to space string ' '
+    return board
 
 def get_player_move(board):
     """
@@ -72,13 +68,30 @@ Choose your square:20 21 22 : ''')
             else:
                 break
         break
-    user_row = int(user_choice_row) # conversion to digit
-    user_column = int(user_choice_column) # conversion to digit
-    user_position = board[user_row][user_column]
+    row = int(user_choice_row) # conversion to digit
+    col = int(user_choice_column) # conversion to digit
+    user_position = board[row][col]
     if not user_position==' ': # checks if user's choice is empty or not
         print('Please choose empty space')
         return (None, None)
-    return user_row, user_column
+    return row, col
+
+def choose_computer_move(board):
+    """
+    This is choose_computer_move.
+    Randomly generate computer's move in a list of all empty spaces.
+    Return row and column.
+    """
+    empty_position = []
+    board_length = len(board)
+    for i in range(board_length): # makes a list of all empty position
+        for j in range(board_length):
+            if board[i][j] == ' ':
+                empty_position.append([i,j])
+    choice = random.choice(empty_position) # randomly chooses an empty places
+    row = choice[0]
+    col = choice[1]
+    return row, col
 
 def check_for_win(board, mark):
     """
@@ -98,7 +111,7 @@ def check_for_win(board, mark):
     for i in range(board_length): # check for column win
         did_win = True
         for j in range(board_length):
-            if board[i][j] != mark:
+            if board[j][i] != mark:
                 did_win = False
                 continue
         if did_win:
@@ -131,22 +144,7 @@ def check_for_draw(board):
             return is_drawn
     return is_drawn
 
-def choose_computer_move(board):
-    """
-    This is choose_computer_move.
-    Randomly generate computer's move in a list of all empty spaces.
-    Return row and column.
-    """
-    empty_position = []
-    board_length = len(board)
-    for i in range(board_length): # makes a list of all empty position
-        for j in range(board_length):
-            if board[i][j] == ' ':
-                empty_position.append([i,j])
-    choice = random.choice(empty_position) # randomly chooses an empty places
-    return choice[0], choice[1]
-
-def play_game():
+def play_game(board):
     """
     This is play_game function.
     Main game is played here.
@@ -187,19 +185,39 @@ Computer's move is''')
             print('Its a draw')
             return 0
 
+def menu():
+    """
+    This is menu function.
+    Prompts user to enter 1, 2, 3, q.
+    Calls other functions accordingly.
+    """
+    user_choice = input('''
+Enter one of the following options: 
+    1 - Play the game
+    2 - Save your score in the leaderboard
+    3 - Load and display the leaderboard
+    q - End the program
+1, 2, 3, or q: ''') # prompt user to input 1, 2, 3, q
+    if user_choice.lower() in ['1', '2', '3', 'q']:
+        return user_choice.lower()
+    else:
+        print('''
+Please print a valid mode''')
+
 def load_scores():
     """
     This is load_scores function.
     Open leaderboard.txt file.
     Use json.loads function to load dictionary.
     """
+    if not exists('leaderboard.txt'):
+        print("\nLeaderboard does not exists.\n")
     with open('leaderboard.txt', 'r', encoding='utf-8') as read_file:
         line = read_file.read()
     leaderboard = json.loads(line) # loads all names and score in leaderboard
     return leaderboard
 
-
-def save_score(user_score):
+def save_score(score):
     """
     This is save_score function.
     Prompt user to input name.
@@ -213,51 +231,21 @@ def save_score(user_score):
     all_players = leaderboard.keys() # only get user names
     if user_name in all_players: # if user in leaderboard
         old_score = leaderboard[user_name]
-        new_score = old_score + user_score # update the old score
+        new_score = old_score + score # update the old score
         leaderboard[user_name] = new_score
     else: # if user not in leaderboard
-        leaderboard[user_name] = user_score
+        leaderboard[user_name] = score
     with open('leaderboard.txt', 'w', encoding='utf-8') as write_file:
         json.dump(leaderboard, write_file) # stores all names and scores
+        print('Saved Successfully.')
 
-def display_leaderboard(users):
+def display_leaderboard(leaders):
     """
     This is display_leaderboard function.
     Diplays all players and their scores in console.
     """
-    for names, scores in users.items(): # loops through all names and scores
-        print(names, scores)
-
-def menu():
-    """
-    This is menu function.
-    Prompts user to enter 1, 2, 3, q.
-    Calls other functions accordingly.
-    """
-    score = None
-    while True:
-        user_choice = input('''
-Enter one of the following options: 
-        1 - Play the game
-        2 - Save your score in the leaderboard
-        3 - Load and display the leaderboard
-        q - End the program
-1, 2, 3, or q: ''') # prompt user to input 1, 2, 3, q
-        if user_choice.lower() == 'q':
-            print('Thanks for playing ')
-            break
-        elif user_choice == '1': # if user_choice is 1 start game
-            score = play_game()
-        if score is None and user_choice == '2':
-            print('''
-Play a game first.''')
-        elif user_choice == '2': # if user_choice is 2 save score
-            save_score(score)
-            score = None
-        elif user_choice == '3': # if user_choice is 3 load score and display
-            leaders = load_scores()
-            display_leaderboard(leaders)
-
-if __name__ == '__main__': # can be called only as a script not as a module
-    menu()
-    
+    print ('''
+======LEADERBOARD======
+''')
+    for names, scores in leaders.items(): # loops through all names and scores
+        print(names , 'scored' , scores)
